cc532394d67af265fda71a0fa65f1538e1062b68
Albert Volkman <albert.volkman@gmail.com>
2022-05-26 22:26:29 -0400

    Add internship pages

---
+++ pages/internships/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container pb-5 mt-4">
+    <h2 class="font-weigth-bolder text-center">Internships</h2>
+    <template v-if="loading">
+      <img
+        style="margin-top: 27vh"
+        class="mx-auto d-block"
+        width="20%"
+        src="~/assets/imgs/loading.svg"
+        alt="Blue Loading Spinner"
+      />
+    </template>
+    <template v-else>
+      <InternshipFeedDisplay />
+    </template>
+  </div>
+</template>
+
+<script>
+import InternshipFeedDisplay from '~/components/Jobs/InternshipFeedDisplay'
+
+export default {
+  name: 'Internships',
+  components: { InternshipFeedDisplay },
+  computed: {
+    loading() {
+      return this.$store.state.loading
+    }
+  }
+}
+</script>
